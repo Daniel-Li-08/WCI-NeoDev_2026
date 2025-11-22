d99d5e08d92bb9a1830d8c1a873aa6064f4a9d1c
Camleaf <bdaace09@gmail.com>
2025-11-22 13:05:17 -0500

    vercel stupid

--- backend-api/main.py
+++ backend-api/main.py
@@ -1,11 +1,12 @@
 from flaskr import create_app
 from flask import Flask
 
+app = create_app()
+
 def main(app:Flask):
 
     app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == "__main__":
-    app = create_app()
     main(app)
