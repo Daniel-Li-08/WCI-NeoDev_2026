f41e6b84d8de02ff1cf42902af9ed823aa580cef
gheatherington <gavinheatherington@gmail.com>
2025-11-22 18:13:10 -0500

    createCart

--- website-portfolio/src/components/CreateCart.jsx
+++ website-portfolio/src/components/CreateCart.jsx
@@ -1,6 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+
 const CreateCart = () => {
-	return <h1>You are logged in as a prime user, would you like to create a new cart?</h1>;
-	
+	const navigate = useNavigate();
+	return (
+		<div>
+			<h1>
+				You are logged in as a prime user, would you like to create a new cart?
+			</h1>
+			<button
+				className="bg-blue-700"
+				onClick={async () => {
+					const response = await fetch(
+						'https://wci-neo-dev-2025api.vercel.app/cart/create',
+						{
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json',
+							},
+							body: JSON.stringify({
+								owner: localStorage.getItem('name'),
+							}),
+						}
+					);
+					if (response.ok) {
+						alert('Cart created successfully!');
+						navigate('/cart');
+					} else {
+						const errorData = await response.text();
+						alert(`Error creating cart: ${errorData}`);
+					}
+				}}
+			>
+				Create Cart
+			</button>
+		</div>
+	);
 };
 
 export default CreateCart;
